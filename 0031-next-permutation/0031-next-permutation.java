e259c0d3e4469cb63efea88c6f12d7e7445b0c7b
Suvetha P T <ptsuvetha12@gmail.com>
2026-08-25 19:19:16 +0530

    Time: 0 ms (100%), Space: 44.1 MB (99.09%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,35 +1,38 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n=nums.length;
-        int pivot=0;
-        int ind=-1;
-        for(int i=n-2;i>=0;i--){
+        int len=nums.length;
+        int pivot=-1;
+        for(int i=len-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                ind=i;
-                pivot=nums[i];
-                break;
-            }
-
-        }
-        if(ind!=-1){
-        for(int i=n-1;i>ind;i--){
-            if(pivot<nums[i]){
-                int t=nums[ind];
-                nums[ind]=nums[i];
-                nums[i]=t;
+                pivot=i;
                 break;
             }
         }
+        if(pivot==-1){
+            reverse(nums,0);
+            return;
         }
-        reverse(nums,ind+1,n-1);
+        int swapind=0;
+        for(int i=len-1;i>=0;i--){
+            if(nums[pivot]<nums[i]){
+                swapind=i;
+                break;
+            }
+        }
+        int t=nums[pivot];
+        nums[pivot]=nums[swapind];
+        nums[swapind]=t;
+        reverse(nums,pivot+1);
     }
-    private static void reverse(int[] nums,int i,int j){
-        while(i<=j){
-            int t =nums[i];
-            nums[i]=nums[j];
-            nums[j]=t;
+    private void reverse(int[] arr,int i){
+        int j=arr.length-1;
+        while(i<j){
+            int t =arr[i];
+            arr[i]=arr[j];
+            arr[j]=t;
             i++;
             j--;
         }
+
     }
 }
